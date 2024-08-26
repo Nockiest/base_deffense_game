d21ed11ffe18589361aa90af11b618c26a0281f3
Andrew <ondralukes06@seznam.cz>
2024-08-26 14:43:10 +0200

    imported music img annd start screen from battleforge

---
+++ screens/EndGameScreen/end_game_screen.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func _on_start_screen_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/screens/StartGameScreen/start_screen.tscn")# 
+
+func _ready():
+	$AudioStreamPlayer.play()
---
+++ screens/StartGameScreen/start_screen.gd
@@ -0,0 +1,24 @@
+extends Control
+
+func _on_start_game_btn_pressed():
+		get_tree().change_scene_to_file("res://screens/battlerground.tscn") 
+
+
+func _on_settings_btn_pressed():
+	show_and_hide($SettingsScreen, $MainButtons)
+ 
+func show_and_hide(node_to_show, node_to_hide):
+	node_to_show.show()
+	node_to_hide.hide()
+
+
+func _on_back_to_start_screen_btn_button_up():
+	show_and_hide($MainButtons, $SettingsScreen)
+
+
+func _on_exit_btn_button_up():
+	get_tree().quit()
+	
+func _ready() -> void:
+	$AudioStreamPlayer.play()
+ 
