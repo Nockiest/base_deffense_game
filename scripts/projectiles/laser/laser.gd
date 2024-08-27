7fc59963a1847658813997dc4a7d5ada5eb3a6b8
Andrew <ondralukes06@seznam.cz>
2024-08-27 09:17:44 +0200

    shortened component names

---
+++ scripts/projectiles/laser/laser.gd
@@ -0,0 +1,2 @@
+class_name Laser
+extends Node2D
--- scripts/turret/turret.gd
+++ scripts/turret/turret.gd
@@ -5,7 +5,11 @@ class_name Turret
 @export var aiming_component: AimingComponent
 @export var ammo_magazine_component: AmmoMagazine
 @export var health_component: HealthComponent
- 
+@export var self_destruction_component: SelfDestructionComponent
  
 
  
+
+
+func _on_health_component_hp_ran_out() -> void:
+	self_destruction_component.kill_owner()
