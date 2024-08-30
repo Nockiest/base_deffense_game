75ba5f35bec3f09cf2c3e53d8ea72ed7e146206c
Andrew <ondralukes06@seznam.cz>
2024-08-30 20:47:46 +0200

    added camera

--- scripts/components/entity_container/object-container.gd
+++ scripts/components/entity_container/object-container.gd
@@ -11,18 +11,18 @@ func recieve_child_node(child_node:Node):
 	var is_valid_type = child_node.is_class(allowed_object_type)
 	var is_within_limit = get_child_count() < max_objects
 
-	oneErr.printerr_once([
+	oneErr.printerr_once("object mismatch", [
 		"Type mismatch:", not is_valid_type,
 		"Maximum objects reached:", not is_within_limit,
 #		"Child type:",  child_node.get_type(),
 		"Allowed type:", allowed_object_type
-	], true)
+	] )
 
 	if  is_valid_type and !is_full() :#is_within_limit and is_valid_type
 		add_child(child_node)
 		emit_signal("objects_changed", get_children())
 	else:
-		oneErr.printerr_once(["Cannot add child node: Type mismatch or maximum number of objects reached", get_children()], true)
+		oneErr.printerr_once( "pbject container cant add node",["Cannot add child node: Type mismatch or maximum number of objects reached", get_children()] )
 
 func is_full() -> bool:
 	return get_child_count() >= max_objects
